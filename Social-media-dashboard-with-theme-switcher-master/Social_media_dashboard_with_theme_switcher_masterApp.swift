f07c45a717b6519af808497a0a9b16c09a4e0f77
mfabianchm <mfchongm@hotmail.com>
2024-02-28 16:16:17 -0600

    Initial Commit

---
+++ Social-media-dashboard-with-theme-switcher-master/Social_media_dashboard_with_theme_switcher_masterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Social_media_dashboard_with_theme_switcher_masterApp.swift
+//  Social-media-dashboard-with-theme-switcher-master
+//
+//  Created by Marcos Fabian Chong Megchun on 28/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Social_media_dashboard_with_theme_switcher_masterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
